fd7dd798e3f120f70f85c94753c4258d6b07d08f
Aaron Hernandez <a.f.h.heroku@gmail.com>
2017-01-13 18:44:57 -0600

    loading node_socket code files

---
+++ server.js
@@ -0,0 +1,71 @@
+var path = require('path');
+var app = require('express')();
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server);
+
+users = [];
+connections = [];
+
+
+var socketPort =  process.env.PORT || 3003;
+
+server.listen(socketPort);
+console.log('Socket.io Server running on port: ' + socketPort);
+
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+
+
+io.sockets.on('connection', function(socket){
+
+	//connect
+	connections.push(socket);
+	console.log('--New user connected--');
+	console.log('Connections: %s sockets connected', connections.length);
+
+	//disconnect
+  socket.on('disconnect', function(data){
+  	users.splice(users.indexOf(socket.username),1);
+  	updateUsernames();
+  	connections.splice(connections.indexOf(socket),1);
+    console.log('--Existing user disconnected--');
+	console.log('Connections: %s sockets connected', connections.length);
+  });
+
+
+  // send message
+  socket.on('send message', function(data){
+
+  		console.log(data);
+  		io.sockets.emit('new message', {msg: data, usr: socket.username});
+
+
+  });
+
+  // new user
+  socket.on('new user', function(data, callback){
+  		if(data.length > 0){
+  			callback(true);
+  			socket.username = data;
+  			users.push(socket.username);
+  			updateUsernames();
+  		}
+  		else{
+  			callback(false);
+  			io.sockets.emit('throw usrError');
+  		}
+
+  		
+  });
+
+
+  function updateUsernames(){
+  	io.sockets.emit('get users', users);
+  }
+});
+
+
+
